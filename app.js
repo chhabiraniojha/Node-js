da4198c397ae920c378961fbd8e4d86968f93a79
chhabiraniojha <chhabiraniojha@gmail.com>
2023-10-03 01:29:50 +0530

    url set with res-home,about,node route

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ const http=require("http");
 
 
 const server=http.createServer((req,res)=>{
-    console.log("suvransu");
+    const url=req.url;
+    if(url==="/"){
+        res.write(
+        "<html><header>my text</header><body>hello</body></html"
+        )
+        res.end()
+        
+    }
+    if(url==="/home"){
+        res.write(
+        "<html><header>my text</header><body>wellcome to home</body></html"
+        )
+        res.end()
+        
+    }
+    if(url==="/about"){
+        res.write(
+        "<html><header>my text</header><body>welcome to abbout us page</body></html"
+        )
+        res.end()
+        
+    }
+    if(url==="/node"){
+        res.write(
+        "<html><header>my text</header><body>welcome to node js project</body></html"
+        )
+        res.end()
+        
+    }
+
 });
 server.listen(4000);
