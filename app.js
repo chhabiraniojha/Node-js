f9b95548f3292042e7d0e2b53e42f53f68d5c920
chhabiraniojha <chhabiraniojha@gmail.com>
2023-10-03 22:25:07 +0530

    form data saved to file also shoing on the screen

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
 const http=require("http");
+const fs=require("fs");
 
 
 const server=http.createServer((req,res)=>{
     const url=req.url;
-    if(url==="/"){
-        res.write(
-        "<html><header>my text</header><body>hello</body></html"
-        )
-        res.end()
-        
-    }
-    if(url==="/home"){
-        res.write(
-        "<html><header>my text</header><body>wellcome to home</body></html"
-        )
-        res.end()
-        
-    }
-    if(url==="/about"){
-        res.write(
-        "<html><header>my text</header><body>welcome to abbout us page</body></html"
-        )
-        res.end()
-        
-    }
-    if(url==="/node"){
-        res.write(
-        "<html><header>my text</header><body>welcome to node js project</body></html"
-        )
-        res.end()
-        
-    }
+    const method=req.method;
+    var message
+    var messages="hiiiiiiii"
+   if(url==="/"){
+    // Use fs.readFile() method to read the file
+    fs.readFile   ('message.txt', {encoding:'utf8'},(err, data)=>{
+      
+    // Display the file content
+    
+     res.write("<html>");
+     res.write("<head></head>");
+     res.write(`<body> ${data}</body>`)
+     res.write("<body>");
+     
+     res.write('<form action="/message" method="POST">');
+     res.write('<input type="text" name="message"><button type="submit">Send</button>');
+     res.write("</form>");
+     res.write("</body>");
+     res.write("</html>");
+     return res.end();
+     });
+     
+   
+   
+    
+   }
+   if(url==="/message"  && method=="POST"){
+     const body=[];
+     req.on("data",(chunk)=>{
+          body.push(chunk);
+     })
+     req.on("end",()=>{
+        const parseBody=Buffer.concat(body).toString();
+        const message=parseBody.split('=')[1];
+        fs.writeFileSync("message.txt",message)
+        res.statusCode=302;
+        res.setHeader("location","/")
+        return res.end();
+     })
+     }
 
 });
 server.listen(4000);
