e901b091fdd3640fa84a8557679c44eef7c601ef
chhabiraniojha <chhabiraniojha@gmail.com>
2023-10-04 21:37:45 +0530

    express installed and middleware used

--- app.js
+++ app.js
@@ -1,50 +1,10 @@
-const http=require("http");
-const fs=require("fs");
+const express=require("express");
+const app=express();
+
+app.use((req,res,next)=>{
+     res.send( { key1: "value" });
+})
 
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    
-   if(url==="/"){
-    // Use fs.readFile() method to read the file
-    fs.readFile   ('message.txt', {encoding:'utf8'},(err, data)=>{
-      
-    // Display the file content
-    
-     res.write("<html>");
-     res.write("<head></head>");
-     res.write(`<body> ${data}</body>`)
-     res.write("<body>");
-     
-     res.write('<form action="/message" method="POST">');
-     res.write('<input type="text" name="message"><button type="submit">Send</button>');
-     res.write("</form>");
-     res.write("</body>");
-     res.write("</html>");
-     return res.end();
-     });
-     
-   
-   
-    
-   }
-   if(url==="/message"  && method=="POST"){
-     const body=[];
-     req.on("data",(chunk)=>{
-          body.push(chunk);
-     })
-     req.on("end",()=>{
-        const parseBody=Buffer.concat(body).toString();
-        const message=parseBody.split('=')[1];
-        fs.writeFileSync("message.txt",message)
-        res.statusCode=302;
-        res.setHeader("location","/")
-        return res.end();
-     })
-     }
 
-     
-
-});
-server.listen(4000);
+app.listen(3000);
