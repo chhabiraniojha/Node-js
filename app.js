bb2a5389c99f54253c7ee935b1c2a26fabbb3792
chhabiraniojha <chhabiraniojha@gmail.com>
2023-10-05 00:56:21 +0530

    body parser installed & printed data on the consol

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express=require("express");
+const bodyParser=require("body-parser")
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req,res,next)=>{
-     res.send( { key1: "value" });
+
+app.use("/addProd",(req,res,next)=>{
+     res.send('<form action="/prod" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit"> add item</button></form>');
+})
+app.post("/prod",(req,res)=>{
+     console.log(req.body);
+     res.redirect("/");
+})
+app.use("/",(req,res,next)=>{
+     res.send("hello world")
 })
 
 
